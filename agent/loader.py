d3a1c435fc16a5de1d5e78a6029eee47dc52657c
forvicky <412909712@qq.com>
2020-05-21 00:27:07 +0800

    base script

--- agent/loader.py
+++ agent/loader.py
@@ -1,6 +1,7 @@
 import time
 import frida
 
+# more devices rpc 
 device = frida.get_device_manager().add_remote_device("192.168.0.108:8888")
 pid=device.spawn("com.android.settings")
 device.resume(pid)
@@ -10,4 +11,15 @@ with open("s1.js") as f:
     script = session.create_script(f.read())
 
 script.load()
+
+device9 = frida.get_device_manager().add_remote_device("192.168.0.109:9999")
+pid9=device9.spawn("com.android.settings")
+device9.resume(pid9)
+time.sleep(1)
+session = device9.attach(pid9)
+with open("s1.js") as f:
+    script = session.create_script(f.read())
+
+script.load()
+
 input()
--- agent/s1.js
+++ agent/s1.js
@@ -1,5 +1,5 @@
-//
-
+//frida -U -f com.example.myapplication -l s1.js --no-pause      //-U usb -f force -l script 
+//frida -U com.example.myapplication -l s1.js                  //delay to find,so can find
 function main(){
     console.log("Entering the Script");
  
@@ -7,12 +7,28 @@ function main(){
         console.log("Inside Java perform");
 
         var MainActivity=Java.use("com.example.myapplication.MainActivity");
-        MainActivity.fun.implementation = function(x,y){
+        MainActivity.fun.overload('int','int').implementation = function(x,y){
             console.log("original call x="+x+" y="+y);
             var ret_value=this.fun(2,5);
             return ret_value;
         }
 
+        MainActivity.fun.overload('java.lang.String').implementation = function(str){
+            console.log("original call str="+str);
+            var newStr=Java.use('java.lang.String').$new("zdd");
+            var ret_value=this.fun(newStr);
+            return ret_value;
+        }
+
+        Java.choose("com.example.myapplication.MainActivity",{
+            onMatch:function(instance) {
+                console.log("found instance="+instance);
+                console.log("Result of func="+instance.secret());
+            },
+            onComplete:function(){
+                console.log("complete");
+            }
+        });
     });
     
 }
