3ae0974580212d1f0855e21bb0b8e50a96c9a5fc
forvicky <412909712@qq.com>
2020-06-23 01:10:22 +0800

    libc hook

--- agent/sohook.js
+++ agent/sohook.js
@@ -276,6 +276,37 @@ function inline_hook(){
 
 }
 
+
+function write_reg(){
+    var file = new File("/sdcard/reg.dat","w");
+    file.write("EoPAoY62@ElRD");
+    file.flush();
+    file.close();
+}
+
+
+function write_reg2(){
+
+    var fopen_addr = Module.findExportByName("libc.so","fopen");
+    var fputs_addr = Module.findExportByName("libc.so","fputs");
+    var fclose_addr = Module.findExportByName("libc.so","fclose");
+
+    console.log("fopen:",fopen_addr," fputs :",fputs_addr," fclose :",fclose_addr);
+
+    var fopen = new NativeFunction(fopen_addr,"pointer",["pointer","pointer"]);
+    var fputs = new NativeFunction(fputs_addr,"int",["pointer","pointer"])
+    var fclose = new NativeFunction(fclose_addr,"int",["pointer"]);
+
+    var filename = Memory.allocUtf8String("/sdcard/reg.dat");
+    var file_mode = Memory.allocUtf8String("w+");
+
+    var file = fopen(filename,file_mode );
+
+    var contents = Memory.allocUtf8String("EoPAoY62@ElRD");
+    var ret = fputs(contents,file);
+    fclose(file);
+}
+
 function main(){
     //hook_java();  //hook java层native函数声明
     //hook_native();
